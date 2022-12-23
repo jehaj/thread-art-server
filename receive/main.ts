64891f6be6010f40b4d2bf6158a9f4b77a7bb342
Nikolaj J-K <kagenogmig@gmail.com>
2022-12-23 19:02:23 +0100

    RandomID as filename for uploaded image and error handling.
    The error handling is a catch-all without trying to correct
    anything - only stating that something went wrong, instead of
    crashing the program :)
    
    The RandomID is inspired by the Go implementation that can be
    found in `thread-art-archive`. Even though the ID is 12 characters
    long, there could occur a collision, where the correct way is
    maybe to tell the user to try again, or automatically generate
    another ID until not colliding.

--- receive/main.ts
+++ receive/main.ts
@@ -1,3 +1,17 @@
+function RandomID() {
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const clen = letters.length;
+  const r = crypto.getRandomValues(new Uint8Array(12));
+  const b = [];
+  
+  for (let i = 0; i < 12; i++) {
+    const c = parseInt(r[i]);
+    b.push(letters[c%clen]);
+  }
+  
+  return b.join('');
+}
+
 const server = Deno.listen({ port: 8001 });
 for await (const conn of server) {
   serveHttp(conn);
@@ -6,12 +20,18 @@ for await (const conn of server) {
 async function serveHttp(conn: Deno.Conn) {
   const httpConn = Deno.serveHttp(conn);
   for await (const requestEvent of httpConn) {
-    const f = await requestEvent.request.formData();
-    const image = await f.get("image").arrayBuffer();
-    console.log(image);
-    await Deno.writeFile('./saves/image.png', image);
-    requestEvent.respondWith(
-      new Response("Success!", { status: 200 })
-    );
+    try {
+      const f = await requestEvent.request.formData();
+      const image = await f.get("image").arrayBuffer();
+      const filename = RandomID();
+      await Deno.writeFile(`./saves/${filename}.png`, image);
+      requestEvent.respondWith(
+        new Response(`Success! Your ID is ${filename}`, { status: 200 })
+      );
+    } catch (error) {
+      requestEvent.respondWith(
+        new Response("Something went wrong! Please wait before trying again.", { status: 500 })
+      );
+    }
   }
 }
