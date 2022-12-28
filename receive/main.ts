5c52a90f0190a0b20e6a80203c666743ff326c06
Nikolaj J-K <kagenogmig@gmail.com>
2022-12-28 22:06:21 +0100

    Limit of 10 in "queue". The image is converted. Change network.
    The network has been changed from serveHttp (native) to serve
    (std/http).

--- receive/main.ts
+++ receive/main.ts
@@ -1,37 +1,66 @@
+import { join } from "https://deno.land/std@0.170.0/path/posix.ts";
+import { serve } from "https://deno.land/std@0.170.0/http/server.ts";
+
 function RandomID() {
-  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const letters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   const clen = letters.length;
   const r = crypto.getRandomValues(new Uint8Array(12));
   const b = [];
-  
   for (let i = 0; i < 12; i++) {
     const c = parseInt(r[i]);
-    b.push(letters[c%clen]);
+    b.push(letters[c % clen]);
   }
-  
-  return b.join('');
+  return b.join("");
 }
 
-const server = Deno.listen({ port: 8001 });
-for await (const conn of server) {
-  serveHttp(conn);
-}
+const QUEUE_PATH = "./queue";
+const QUEUE_LIMIT = 10;
 
-async function serveHttp(conn: Deno.Conn) {
-  const httpConn = Deno.serveHttp(conn);
-  for await (const requestEvent of httpConn) {
-    try {
-      const f = await requestEvent.request.formData();
-      const image = await f.get("image").arrayBuffer();
-      const filename = RandomID();
-      await Deno.writeFile(`./saves/${filename}.png`, image);
-      requestEvent.respondWith(
-        new Response(`Success! Your ID is ${filename}`, { status: 200 })
-      );
-    } catch (error) {
-      requestEvent.respondWith(
-        new Response("Something went wrong! Please wait before trying again.", { status: 500 })
-      );
-    }
+serve(handler, { port: 8001 });
+
+async function handler(req: Request): Response {
+  const d = Deno.readDir("./queue");
+  let sum = 0;
+  for await (const _ of d) {
+    sum += 1;
+  }
+  if (sum >= QUEUE_LIMIT) {
+    return new Response("Try again later!", { status: 500 });
+  }
+  try {
+    const f = await req.formData();
+    const image = await f.get("image").arrayBuffer();
+    const filename = RandomID();
+    await Deno.mkdir(`./saves/${filename}`);
+    await Deno.writeFile(`./saves/${filename}/INPUT.png`, image, {
+      createNew: true,
+    });
+    const cmd = [
+      "convert",
+      `./saves/${filename}/INPUT.png`,
+      "-resize",
+      "400x400^",
+      "-gravity",
+      "center",
+      "-extent",
+      "400x400",
+      "-colorspace",
+      "Gray",
+      `./saves/${filename}/OUTPUT.png`,
+    ];
+    
+    const p = Deno.run({ cmd: cmd });
+    await p.status();
+    await Deno.writeTextFile(join(QUEUE_PATH, filename));
+    p.close();
+    
+    return new Response(`Success! Your ID is ${filename}`, { status: 200 });
+    
+  } catch (error) {
+    return new Response(
+      "Something went wrong! Please wait before trying again.",
+      { status: 500 },
+    );
   }
 }
