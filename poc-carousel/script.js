67d33a2e7644e75c52aa85cedbd24e6de15c849b
Nikolaj J-K <kagenogmig@gmail.com>
2023-02-24 20:45:49 +0100

    carousel demo

---
+++ poc-carousel/script.js
@@ -0,0 +1,19 @@
+async function main() {
+    console.log("hello world!");
+    const data = await fetch("/example.txt");
+    const result = (await data.text()).split('\n')[1].split(',');
+    
+    const carousel = document.getElementById("carousel");
+    for (let i = 0; i < result.length-1; i++) {
+        const step = document.createElement("div");
+        const point = result[i];
+        step.id = "step-" + (i+1);
+        step.innerText = point;
+        const stepnumber = document.createElement("p");
+        stepnumber.innerText = (i+1);
+        step.appendChild(stepnumber);
+        carousel.appendChild(step);
+    }
+}
+
+main();
