7e6dd8cd87442342ba7638cd5e9c8251abc3e593
Nikolaj J-K <kagenogmig@gmail.com>
2024-05-06 18:56:55 +0200

    init new project

--- act/deps.ts
+++
@@ -1,2 +0,0 @@
-export { join } from "https://deno.land/std@0.170.0/path/mod.ts";
-export { Worker, Job } from "npm:bullmq";
--- receive/deps.ts
+++
@@ -1,4 +0,0 @@
-export { basename, join } from "https://deno.land/std@0.170.0/path/mod.ts";
-export { serve } from "https://deno.land/std@0.170.0/http/server.ts";
-export { loadSync } from "https://deno.land/std@0.170.0/dotenv/mod.ts";
-export { Queue } from "npm:bullmq"
